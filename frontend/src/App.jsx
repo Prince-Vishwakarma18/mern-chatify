6c7e17f5fb7c5ea383ae4d0f7da156c1e7a11fee
Prince Vishwakarma <prince.vishwakarma392@gmail.com>
2025-12-04 00:02:57 +0530

    Fix React Router for Render static hosting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,22 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Hero from "./pages/Hero";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Hero />,
-    },
-    {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "/signup",
-        element: <Signup />,
-    },
-    {
-        path: "/home",
-        element: <Home />,
-    },
-    {
-        path: "/profile",
-        element: <Profile />,
-    },
-]);
-
 function App() {
     return (
-        <>
-            <RouterProvider router={router} />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Hero />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
