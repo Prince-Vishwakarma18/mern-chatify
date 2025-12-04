1c2ae3aaf2d820a74e066ecedef63e86f35a9aa0
Prince Vishwakarma <prince.vishwakarma392@gmail.com>
2025-12-04 19:31:29 +0530

    Fix CORS, cookies, and production URLs for Render

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+        origin: [ process.env.FRONTEND_URL],
         credentials: true,
     })
 );
